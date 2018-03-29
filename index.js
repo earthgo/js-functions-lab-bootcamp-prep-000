2dc6e861b316589c7e00a42f3915e03e6e9c008d
emy garcia <emygarcia1920@gmail.com>
2018-03-29 22:12:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays() {
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo(){
+function happyHolidayTo(holiday){
   return `Happy ${holiday}, ${name}!`
 }
